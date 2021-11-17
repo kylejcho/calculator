1576317a4f7098665447b4d521c72f3a4b27f15c
Kyle Cho <kylejcho@gmail.com>
2021-11-17 17:10:21 -0500

    Design

---
+++ app.js
@@ -0,0 +1,13 @@
+let currentOperand
+let lastOperand
+let operater = "+";
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operate = () => {
+ 
+    
+}
