850cd62a9941009239c180571cc5da368f432837
Kyle Cho <kylejcho@gmail.com>
2021-11-17 21:52:55 -0500

    Current Operand Display

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
-const one = document.querySelector('.1')
+let currentOperand = [0];
+let lastOperand;
+let input;
+let numInput;
+let currentDisplay = '';
+
+const numKey = document.querySelectorAll('.num');
+const operator = document.querySelectorAll('.operator');
+const result = document.querySelector('.result');
+const equalButton = document.querySelector('#equalKey');
+const plusButton = document.querySelector('#plusButton');
+
+document.onclick = (event) => {
+    let key = event.target;
+    if (key.className === "key num") {
+        numInput = key.innerHTML;
+        newNumInput();
+    }
+}
+
+
+const newNumInput = () => {
+    if (currentOperand[0] === 0) {
+        currentOperand[0] = numInput;
+        console.log(currentOperand);
+    } else {
+        currentOperand.push(numInput);
+        console.log(currentOperand);
+    }
+    updateOperand();
+}
+
+const updateOperand = () => {
+     result.innerHTML = currentOperand.join('');
+} 
+
+
+const createOperand = () => {
+    lastOperand = currentOperand.join('');
+    console.log(lastOperand)
+}
+
+
+plusButton.onclick = () => {
+    createOperand();
+}
 
 
 
 
-
-
-
-
-let currentOperand
-let lastOperand
-let operater = "+";
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
