f475c338044e0a28aed9906852ff117ea7994c0f
Kyle Cho <kylejcho@gmail.com>
2021-11-19 17:31:05 -0500

    Complete operator functionality

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let operandArray = [];
 let operation;
 let numInput;
-let a;
-let b;
+let a = '';
+let b = '';
 
 const numKey = document.querySelectorAll('.num');
 const operator = document.querySelectorAll('.operator');
@@ -16,16 +16,24 @@ const historyDisplay = document.querySelector('.history');
 document.onclick = (e) => {
     let target = e.target;
     if (target.className == "key num") newNumInput(e);
-    else if (target.className == "key operator" && operandArray.length > 0) {
-        if (target.innerHTML !== "="){
+    
+    else if (target.className == "key operator" && operandArray.length || a > 0) {
+        createNewOperand();
+        if (target.innerHTML !== "=" && b == ''){
             operation = target.innerHTML;
             console.log("operation set to " + operation);
         } 
-        createNewOperand();
+        if (target.innerHTML !== "=" && b != '') {
+            console.log("operation set to " + operation);
+            evaluate();
+            operation = target.innerHTML;
+        }
+
+        //createNewOperand();
+
         if (target.innerHTML == "="){
             evaluate();
         }
-        //createNewOperand();
     }
 }
 
@@ -37,7 +45,7 @@ const newNumInput = (e) => {
 }
 
 const createNewOperand = () => {
-    if (!a) a = operandArray.join('');
+    if (a == '') a = operandArray.join('');
     else b = operandArray.join('');
     console.log("a = " + a,'\n',"operandArray =", operandArray,'\n',"b = " + b);
     operandArray = [];
@@ -45,16 +53,16 @@ const createNewOperand = () => {
 }
 
 
-/*equalButton.onclick = () => {
-    evaluate();
-}
-*/
-
 const evaluate = () => {
     a = Number(a);
     b = Number(b);
     operate(a, b, operation);
     console.log(a + " " + operation + " " + b + " = "  + operate(a, b, operation));
+    resultDisplay.innerHTML = operate(a, b, operation);
+    a = operate(a, b, operation);
+    b = '';
+    console.log("a = " + a);
+    console.log("b = " + b);
 }
 
 
@@ -72,6 +80,8 @@ clearButton.onclick = () => {
     operandArray = [];
     resultDisplay.innerHTML = 0;
     historyDisplay.innerHTML = "";
+    a = '';
+    b = '';
     console.clear();
 }
 
