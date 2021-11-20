6999fbfa95635c954976dc280515bb940f68c5bf
Kyle Cho <kylejcho@gmail.com>
2021-11-20 00:58:16 -0500

    Full Operator functionality

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ let a = '';
 let b = '';
 let fontSize = 40;
 
-
 const keypad = document.querySelector('.keypad');
 const equalButton = document.querySelector('#equalButton');
 const plusButton = document.querySelector('#plusButton');
 const minusButton = document.querySelector('#minusButton');
 const clearButton = document.querySelector('#clearButton');
+const squareButton = document.querySelector('#squareButton');
 const resultDisplay = document.querySelector('.resultDisplay');
 const historyDisplay = document.querySelector('.history');
 
+
 keypad.onclick = (e) => {
     let target = e.target;
-    if (target.className == "key num" ) newNumInput(e)
-    else if (target.className == "key operator" && operandArray.length || a > 0) {
+    if (target.className == "key num" && historyDisplay.innerHTML.includes("=")) clear();
+    if (target.className == "key num" ) newNumInput(e);
+
+    else if (target.className == "key operator" && operandArray.length || target.className == "key operator" && a != 0 ) {
         createNewOperand();
         if (target.innerHTML !== "=" && b == ''){
             operation = target.innerHTML;
@@ -30,8 +33,11 @@ keypad.onclick = (e) => {
             operation = target.innerHTML;
             historyDisplay.innerHTML = a + " " + operation;
         }
-        if (target.innerHTML == "="){
+        if (target.innerHTML == "=" && b != ''){
             evaluate();
+            
+        } else if (target.innerHTML == "=" && b == '') {
+            historyDisplay.innerHTML = resultDisplay.innerHTML;
         }
     }
 }
@@ -57,13 +63,14 @@ const evaluate = () => {
     a = Number(a);
     b = Number(b);
     historyDisplay.innerHTML += ' ' + b + ' =';
-    let answer = Math.round(operate(a, b, operation)*10000000000)/10000000000;
+    let answer = Math.round(operate(a, b, operation)*1000000)/1000000;
     console.log(a + " " + operation + " " + b + " = "  + answer);
     resultDisplay.innerHTML = answer;
     a = answer;
     b = '';
     console.log("a = " + a);
     console.log("b = " + b);
+    console.log("answer = " + answer);
 }
 
 
@@ -78,6 +85,10 @@ const resize = () => {
 }
 
 clearButton.onclick = () => {
+    clear();
+}
+
+const clear = () => {
     operandArray = [];
     resultDisplay.innerHTML = 0;
     historyDisplay.innerHTML = "";
@@ -87,18 +98,41 @@ clearButton.onclick = () => {
 }
 
 
-
+squareButton.onclick = () => {
+    if (a=='' && b =='') {
+        createNewOperand();
+        a = Number(a);
+        b = a;
+        historyDisplay.innerHTML += ' sqr(' + b + ') =';
+        let answer = Math.round(operate(a, b, operation)*1000000)/1000000;
+        console.log(b + "^2 = "  + answer);
+        resultDisplay.innerHTML = answer;
+        a = answer;
+        b = '';
+    } else {
+        createNewOperand();
+        a = Number(a);
+        historyDisplay.innerHTML = a + ' ' + operation + ' sqr(' + b + ') =';
+        b = Math.round((b*b)*1000000)/1000000;
+        let answer = Math.round(operate(a, b, operation)*1000000)/1000000;
+        a = answer;
+        resultDisplay.innerHTML = answer;
+        b = '';
+    }
+}
 
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const square = (a) => a * b;
 
 const operate = (a, b, operation) => {
     if (operation == "+") return add(a, b);
     if (operation == "-") return subtract(a, b);
     if (operation == "×") return multiply(a, b);
     if (operation == "÷") return divide(a, b);
+    if (operation == "2" || "x" || "<i>x</i><sup>2</sup>") return square(a);
 }
 
