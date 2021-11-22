abb966e7dee3f58f6bc73d37e866d01ae5bd8450
Kyle Cho <kylejcho@gmail.com>
2021-11-22 01:58:25 -0500

    Readability

--- app.js
+++ app.js
@@ -9,127 +9,143 @@ const clearButton = document.querySelector('#clearButton');
 const squareButton = document.querySelector('#squareButton');
 const deleteButton = document.querySelector('#deleteButton');
 const signButton = document.querySelector('#signButton')
-const resultDisplay = document.querySelector('.resultDisplay');
+const display = document.querySelector('.resultDisplay');
 const history = document.querySelector('.history');
-
 
 keypad.onclick = (e) => {
     let t = e.target;
-    if (resultDisplay.innerHTML == "Error") clear();
-    else if (t.className == "key num") {
-        if (history.innerHTML.includes("=") || history.innerHTML == resultDisplay.innerHTML) clear();
-        newNumInput(e);
-    } else if (t.className == "key operator" && resultDisplay.innerHTML.length>0) {
-        if (resultDisplay.innerHTML == 0) operandArray = [0];
-        createNewOperand();
-        if (t.innerHTML == "="){
-            if (b == '') history.innerHTML = resultDisplay.innerHTML;
-            else evaluate();
-        } else if (b == '') {
-            operation = t.innerHTML;
-            history.innerHTML = a + " " + operation;
-        }  else {
-            evaluate();
-            operation = t.innerHTML;
-            history.innerHTML = a + " " + operation;
-        }
+    if (display.innerHTML == "Error") {
+        clear();
+    } if (t.className == "key num") {
+        numClick(e);
+    } if (t.className == "key operator" && display.innerHTML.length > 0) {
+        operatorClick(e);
     }
 }
 
-const newNumInput = (e) => {
-    numInput = e.target.innerHTML;
-    operandArray.push(numInput);
-    resultDisplay.innerHTML = operandArray.join('');
+const numClick = (e) => {
+    if (history.innerHTML.includes("=") || history.innerHTML == display.innerHTML) {
+        clear();
+    }
+    operandArray.push(e.target.innerHTML);
+    display.innerHTML = operandArray.join('');
+}
+
+const operatorClick = (e) => {
+    if (display.innerHTML == 0) {
+        operandArray = [0];
+    }
+    createNewOperand();
+    if (e.target.innerHTML == "=") {
+        if (b == '') {
+            history.innerHTML = display.innerHTML;       
+        } else {
+            evaluate();
+        }
+    } else if (b == '') {
+        adjustOperation(e);
+    } else {
+        evaluate();
+        adjustOperation(e);
+    }
 }
 
 const createNewOperand = () => {
-    if (a == '') a = operandArray.join('');
-    else b = operandArray.join('');
+    if (a == '') {
+        a = operandArray.join('');
+    } else {
+        b = operandArray.join('');
+    }
     operandArray = [];
 }
-
 
 const evaluate = () => {
     a = Number(a);
     b = Number(b);
     history.innerHTML += ' ' + b + ' =';
     a = Math.round(operate(a, b, operation)*1000000)/1000000;
-    resultDisplay.innerHTML = a;
+    display.innerHTML = a;
     b = '';
-    if (resultDisplay.innerHTML == "NaN" || resultDisplay.innerHTML == "Infinity") resultDisplay.innerHTML = "Error";
+    if (display.innerHTML == "NaN" || display.innerHTML == "Infinity") {
+        display.innerHTML = "Error";
+    }
 }
 
-
-clearButton.onclick = () => {
-    clear();
+const adjustOperation = (e) => {
+    operation = e.target.innerHTML;
+    history.innerHTML = a + " " + operation;
 }
 
 const clear = () => {
     operandArray = [];
-    resultDisplay.innerHTML = 0;
+    display.innerHTML = 0;
     history.innerHTML = "";
     a = '';
     b = '';
 }
 
+clearButton.onclick = () => clear();
 
 squareButton.onclick = () => {
     if (a=='') {
         createNewOperand();
         a = Number(a);
-        if (history.innerHTML.length > 0) history.innerHTML = ' sqr(' + a + ') =';
-        else history.innerHTML += ' sqr(' + a + ') =';
+        if (history.innerHTML.length > 0) {
+            history.innerHTML = ' sqr(' + a + ') =';
+        } else {
+            history.innerHTML += ' sqr(' + a + ') =';
+        }
         a = Math.round((a*a)*1000000)/1000000;
-        resultDisplay.innerHTML = a;
+        display.innerHTML = a;
     } else if (a != '' && b == '' && history.innerHTML.includes("=") || a != '' && !history.innerHTML.includes(" ")) {
         a = Number(a);
         history.innerHTML = ' sqr(' + a + ') =';
         a = Math.round((a*a)*1000000)/1000000;
-        resultDisplay.innerHTML = a;
+        display.innerHTML = a;
     } else {
         createNewOperand();
         a = Number(a);
         history.innerHTML = a + ' ' + operation + ' sqr(' + b + ') =';
         b = b*b;
         a = Math.round(operate(a, b, operation)*1000000)/1000000;
-        resultDisplay.innerHTML = a;
+        display.innerHTML = a;
         b = '';
     }
 }
 
-
 deleteButton.onclick = () => {
     a = '';
-    let result = resultDisplay.innerHTML;
-    resultDisplay.innerHTML = result.substring(0, result.length - 1);
-    if (resultDisplay.innerHTML.length > 0) operandArray.pop();
-    if (operandArray.length = 1) operandArray = resultDisplay.innerHTML.split('');
-}
-
-
-signButton.onclick = () => {
-    if (operandArray.length > 0) {
-        if (resultDisplay.innerHTML.includes('-')) operandArray.shift();
-        else operandArray.unshift("-");
-        resultDisplay.innerHTML = operandArray.join('');
-    } else {
-        a = -1 * a;
-        resultDisplay.innerHTML = a;
+    const result = display.innerHTML;
+    display.innerHTML = result.substring(0, result.length - 1);
+    if (display.innerHTML.length > 0) {
+        operandArray.pop();
+    } if (operandArray.length = 1) {
+        operandArray = display.innerHTML.split('');
     }
 }
 
+signButton.onclick = () => {
+    if (operandArray.length > 0) {
+        if (display.innerHTML.includes('-')) {
+            operandArray.shift();
+        } else {
+            operandArray.unshift("-");
+        }
+        display.innerHTML = operandArray.join('');
+    } else {
+        a = -1 * a;
+        display.innerHTML = a;
+    }
+}
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-const square = (a) => a * b;
 
 const operate = (a, b, operation) => {
     if (operation == "+") return add(a, b);
     if (operation == "-") return subtract(a, b);
     if (operation == "×") return multiply(a, b);
     if (operation == "÷") return divide(a, b);
-    if (operation == "2" || "x" || "<i>x</i><sup>2</sup>") return square(a);
 }
-
